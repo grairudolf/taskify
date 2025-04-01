db687e62159038576d968af756dc89c4ea680ce1
Grai Rudolf <rtech777r@gmail.com>
2025-04-01 17:57:21 +0100

    Updated taskify

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Analytics from './pages/Analytics';
+import Calendar from './pages/Calendar';
 
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { TaskProvider } from "@/contexts/TaskContext";
-import Index from "./pages/Index";
-import Tasks from "./pages/Tasks";
-import Projects from "./pages/Projects";
-import Analytics from "./pages/Analytics";
-import Calendar from "./pages/Calendar";
-import NotFound from "./pages/NotFound";
-
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <TaskProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/tasks" element={<Tasks />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/analytics" element={<Analytics />} />
-            <Route path="/calendar" element={<Calendar />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TaskProvider>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/analytics" element={<Analytics />} />
+      <Route path="/calendar" element={<Calendar />} />
+    </Routes>
+  );
+}
 
 export default App;
